c24f7d5080740421d9c96876915b048229aa3d4a
袁海雄 <yuanhaixiong@wepiao.com>
2017-01-03 02:11:30 +0800

    首页基本已开发完，还差“筛选”和“更多指数”，详情页静态页已做，影片信息数据没找到，（详情页坑大，之前没看仔细，一个标签就相当一个页面，要减需求不然不能按时上线，图表Echarts不能引入导致小程序里的图表没有H5好看，看到这个版本给来电话说吧）。。（）

--- ebotapp/pages/index/index.js
+++ ebotapp/pages/index/index.js
@@ -1,4 +1,7 @@
 //index.js
+let utils = require('../../utils/util.js')
+let model = require('../../utils/model.js');
+let _ = require('../../utils/underscore.modified.js');
 Page({
     data: {
         background: ['green', 'red', 'yellow'],
@@ -6,8 +9,77 @@ Page({
         vertical: false,
         autoplay: true,
         interval: 3000,
-        duration: 1200
+        duration: 800,
+        date: utils.formatTime( new Date ),
+        movie_list: [],
+        totalBoxOffice: 0,
+        totalBoxOfficeUnits: '',
+        comingMovieList: [],
+        movielineAndCityClass: 'm-hide',
+        boxofficeOptionClass: 'm-hide'
     },
+    onLoad: function () {
+        this._loadData();
+        this.loadComingMovieListTop();
+    },
+    _loadData: function(){
+        wx.showNavigationBarLoading();
+        let that = this,
+            toDay = this.data.date;
+        this.pageIndex = 1;
+        
+        var param = {
+            _IsChart: 0,
+            _Order: 201,
+            _OrderType: 'DESC',
+            _PageIndex: this.pageIndex,
+            _PageSize: 50,
+            _Date: toDay,
+            _DateSort: 'Day',
+            _sDate: toDay,
+            _eDate: toDay,
+            _Line: '',
+            _City: '',
+            _CityLevel: '',
+            _Index: ['101,102,201,225,606'],
+            r: Math.random()
+        };
+        
+        model.post("/Movie/GetIndex_List", param, function (result, msg) {
+            wx.hideNavigationBarLoading();
+            let movie_list = result.data2,
+                movieListLen = movie_list.length,
+                _totalBoxOffice = utils.getHundredMillion(result.data1[0].TotalBoxOffice),
+                item, columnList;
+            for(let i = 0; i < movieListLen; i++){
+                item = movie_list[i];
+                columnList = item.ColumnList ?  item.ColumnList.split('|') : [];
+                item.movieName = columnList[2];
+                item.movieTotalBoxOffices = utils.getHundredMillion(columnList[4]);
+                // item.Attendance = item.Attendnce.toFixed(1);
+                item.BoxOffice = utils.getHundredMillion(item.BoxOffice, '万');
+                item._columnList = columnList;
+                movie_list[i] = item;
+            }
+            that.setData({
+                totalBoxOffice: _totalBoxOffice.num,
+                totalBoxOfficeUnits: _totalBoxOffice.units,
+                movie_list: movie_list
+            })
+        });
+    },
+    loadComingMovieListTop: function(){
+        let that = this;  
+        var param = {
+            r: Math.random()
+        };
+        model.post("/Movie/ComingMovieListTop", param, function (result, msg) {
+            that.setData({
+                comingMovieList: result.data1
+            })
+        })
+    },
+    
     changeIndicatorDots: function (e) {
         this.setData({
             indicatorDots: !this.data.indicatorDots
@@ -32,5 +104,72 @@ Page({
         this.setData({
             duration: e.detail.value
         })
+    },
+    //切换日期
+    bindDateChange: function(e){
+        this.setData({
+            date:e.detail.value
+        })
+        this._loadData();
+    },
+    // 前一天
+    tapPrevDay: function (e) {
+        let day = utils.prevDay(this.data.date);
+        this.setData({
+            date: day
+        })
+        this._loadData();
+    },
+    // 后一天
+    tapNextDay: function (e){
+        let day = utils.nextDay(this.data.date);
+        this.setData({
+            date: day
+        })
+        this._loadData();
+    },
+    // 跳转到影片详情页
+    gotomoviedetail: function(e){
+        let el = e.currentTarget,
+            movie_no = el.id;
+        wx.navigateTo({
+            url: '../moviedetail/moviedetail?movie_no=' + movie_no
+        })
+    },
+    // 监听点击筛选按钮
+    tapShowScreen: function(e) {
+        this.setData({
+            movielineAndCityClass: ''
+        })
+    },
+    tapHideScreen: function(e) {
+        this.setData({
+            movielineAndCityClass: 'm-hide',
+            boxofficeOptionClass: 'm-hide'
+        })
+    },
+    // 监听点击更多指标按钮
+    tapBoxofficeOptionShow: function(e) {
+        this.setData({
+            boxofficeOptionClass: ''
+        })
+    },
+    tapBoxofficeOptionHide: function(e) {
+        this.setData({
+            boxofficeOptionClass: 'm-hide'
+        })
+    },
+    // 监听下拉刷新
+    onPullDownRefresh: function () {
+        wx.stopPullDownRefresh();
+        this.onLoad();
+    },
+    // 分享
+    onShareAppMessage: function () {
+        return {
+            title: '艺恩电影智库',
+            desc: '影片票房列表',
+            path: '/pages/index/index?id=123'
+        }
     }
 })
--- ebotapp/utils/util.js
+++ ebotapp/utils/util.js
@@ -1,4 +1,4 @@
-function formatTime(date) {
+function formatTime(date, isHour) {
   var year = date.getFullYear()
   var month = date.getMonth() + 1
   var day = date.getDate()
@@ -7,8 +7,12 @@ function formatTime(date) {
   var minute = date.getMinutes()
   var second = date.getSeconds()
 
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  if(isHour){
+    return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  }else{
+    return [year, month, day].map(formatNumber).join('-')
+  }
+  
 }
 
 function formatNumber(n) {
@@ -16,6 +20,105 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
+function getHundredMillion(num, _units, numstr) {
+    if(!_units){
+        if (num >= 100000000) {
+            return {
+                num: (num / 100000000).toFixed(2),
+                units: '亿'
+            }
+        } else {
+            return {
+                num: getFormattedNum((num / 10000).toFixed(1)),
+                units: '万'
+            }
+        }
+    }else{
+        return getFormattedNum((num / 10000).toFixed(1));
+    }
+}
+/**
+ * @param {Date}
+ * @return {String} 格式化字符串
+ */
+function getDateStr (_date) {
+    var _y = _date.getFullYear(),
+        _m = _date.getMonth() + 1, //  好坑，从0开始
+        _m = _m < 10 ? "0" + _m : _m,
+        _d = _date.getDate(),
+        _d = _d < 10 ? "0" + _d : _d,
+        _s = _y + "-" + _m + "-" + _d;
+    return _s;
+}
+
+/*获取选择日期距当前日期相差天数
+    @_nowDate 必填 当前日期
+    @_selectDate 必选 为选择日期
+*/
+function restrictSwitchToDate(_selectDate, _nowDate) {
+    var nowDate = _nowDate ? new Date(_nowDate) - 1 + 1 : new Date() - 1 + 1;
+    var selectDate = new Date(_selectDate) - 1 + 1;
+    var _day = 1 * 24 * 60 * 60 * 1000;
+    var _days;
+    if (nowDate > selectDate) {
+        _days = -Math.floor((nowDate - selectDate) / _day);
+    } else if (nowDate < selectDate) {
+        _days = Math.floor((selectDate - nowDate) / _day);
+    } else {
+        _days = 0;
+    }
+    return _days
+}
+// 返回前一天
+function prevDay(date) {
+    var mydate = new Date(date);
+    var resultdate = new Date(mydate - 1 - 1 * 24 * 60 * 60 * 1000);
+    return getDateStr(resultdate);
+}
+
+// 返回后一天
+function nextDay(date) {
+    var mydate = new Date(date);
+    var resultdate = new Date(mydate - 1 + 1 * 24 * 60 * 60 * 1000);
+    return getDateStr(resultdate);
+}
+
+// 返回以date的前后index天
+function getIndexDaysStr(date, index) {
+    var mydate = new Date(date);
+    var resultdate = new Date(mydate - 1 + index * 24 * 60 * 60 * 1000);
+    return getDateStr(resultdate);
+}
+
+// 判断两个日期是同一天
+function isEqualDays(day1, day2) {
+    var _day1 = new Date(day1);
+    var _day2 = new Date(day2);
+    var _y1 = _day1.getFullYear(),
+        _m1 = _day1.getMonth(),
+        _d1 = _day1.getDate(),
+        _y2 = _day2.getFullYear(),
+        _m2 = _day2.getMonth(),
+        _d2 = _day2.getDate();
+    if (_y1 == _y2 && _m1 == _m2 && _d1 == _d2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getFormattedNum (num) {
+    // make sure it is String
+    return String(num).replace(/\B(?=(?:\d{3})+\b)/g, ',');
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  getHundredMillion: getHundredMillion,
+  restrictSwitchToDate: restrictSwitchToDate,
+  prevDay: prevDay,
+  nextDay: nextDay,
+  getIndexDaysStr: getIndexDaysStr,
+  isEqualDays: isEqualDays,
+  getFormattedNum: getFormattedNum
 }
